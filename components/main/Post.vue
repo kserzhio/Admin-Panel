a0fcd9b0011063a13e33ab9c935e5ad73ddc617b
kserzhio <kserzhio@gmail.com>
2019-07-25 22:31:25 +0300

    new app

--- components/main/Post.vue
+++ components/main/Post.vue
@@ -1,19 +1,59 @@
 <template>
-    <article class="post">
-
-    </article>
+   <el-card
+           shadow="hover"
+           body-style="{padding:0}"
+           class="post"
+   >
+       <header slot="header" class="post-header">
+           <h3>Post Title</h3>
+           <small>
+               <i class="el-icon-time"></i>
+               {{new Date().toLocaleString()}}
+           </small>
+       </header>
+       <div class="post-body">
+           <img src="https://www.tripzaza.com/ru/destinations/files/2017/09/Berlin-e1505798693967.jpg" class="post-img" align="post image">
+       </div>
+       <footer class="post-footer">
+            <el-button
+                    round
+                    @click="openPost"
+            >Open</el-button>
+           <span>
+               <i class="el-icon-message">12</i>
+           </span>
+       </footer>
+   </el-card>
 </template>
 
 <script>
     export default {
-        name: "AppPost"
+        name: "AppPost",
+        methods:{
+            openPost(){
+
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
 .post{
-    height: 200px;
-    border:1px solid black;
-    margin-bottom: 2rem;
+    margin-bottom: 1.5rem;
 }
+    .post-header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .post-img{
+        width: 100%;
+        height: auto;
+    }
+    .post-footer{
+            display: flex;
+           justify-content: space-between;
+            align-items: center;
+            padding: 1rem;
+    }
 </style>
