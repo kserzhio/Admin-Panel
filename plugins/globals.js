c6e4247907b5cd89e94a5c2a3ffa26845d5eea5e
kserzhio <kserzhio@gmail.com>
2019-07-29 19:14:13 +0300

    fixed bug

--- plugins/globals.js
+++ plugins/globals.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueMarkdown from 'vue-markdown'
 import DateFilter from '@/comman/date.filter'
-if (process.browser) {
+if (process.client) {
     const ElementUI = require('element-ui')
     const locale = require('element-ui/lib/locale/lang/ru-RU')
     Vue.use(ElementUI, { locale })
